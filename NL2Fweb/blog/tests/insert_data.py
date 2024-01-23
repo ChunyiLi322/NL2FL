4bea79762e7e93a4e3a889b76ca528bdb5dccae6
chunyi <37956130+ChunyiLi322@users.noreply.github.com>
2024-01-23 16:06:47 +0800

    Delete NL2Fweb directory

--- NL2Fweb/blog/tests/insert_data.py
+++
@@ -1,8 +0,0 @@
-from blog.models.formalMethod import FormalMethod
-
-formalmethod = FormalMethod(name='LTL')
-formalmethod.save()
-formalmethod = FormalMethod(name='CTL')
-formalmethod.save()
-formalmethod = FormalMethod(name='PPTL')
-formalmethod.save()
